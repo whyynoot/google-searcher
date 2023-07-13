29c07842689a0d8f537055865b6fcb2b1452be84
whyynot <mlganarchist@gmail.com>
2023-07-13 14:02:10 +0300

    Improved code readability, stability

--- src/ac_searcher/ac_searcher.py
+++ src/ac_searcher/ac_searcher.py
@@ -12,21 +12,14 @@ from dash.dependencies import Input, Output, State
 from dash import dash_table
 import pandas as pd
 import dash_bootstrap_components as dbc
+from dash_styles import style_header, style_cell, style_data_conditional, style
 
+# главный объект приложения. Объекта класса AcSearcher. Будет проинициализироавн вместе с созданием класса AcSearcher.
+# Нужен этот класс для того, чтобы callbackу Dash было к кому обращаться
 web_app: object = None
 
-#app = dash.Dash(external_stylesheets=[dbc.themes.GRID])
-#external_stylesheets=['assets/typography.css']
+# Инициализируем даш, для последующей работы с ним, настраиваем калбеки даша.
 app = dash.Dash()
-# app.css.config.serve_locally = True
-# external_scripts = True
-# app.css.append_css({
-#     'external_url': app.get_asset_url('header.css')
-# })
-#external_stylesheets=app.get_asset_url['assets/typography.css']
-
-
-
 @app.callback(
     Output('result-container', 'children'),
     [Input('submit-button', 'n_clicks')],
@@ -37,6 +30,11 @@ def func(n_clicks, query):
         print("Webapp not initialized")
     else:
         return web_app.process_query(n_clicks, query)
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+app.css.append_css({
+    'external_url': app.get_asset_url('assets/typography.css')
+})
 
 
 # AcSearcher класс для описания программы, ее инициализации и запуска работы
@@ -50,38 +48,66 @@ class AcSearcher:
 
     # Инициализация главного класса приложения. На вход подается конфигурация, от конфигурации мы меняем некоторые настройки
     # классов инициализируемых далее
-    # TODO: Разобраться с инициализацией Dash так, чтобы он был внутри этого класса. И даш можно было бы запускать на основе его.
     def __init__(self, config):
         self.interpreter = ExampleInterpreter()
         searcher_settings = SearcherSettings(postfixes=config["cities"], pages=config["pages_to_scan"])
         self.searchers.append(GoogleSearcher(searcher_settings))
         self.parser_html = ParserHTML(config)
-
-    # Метод запуска даш.
-    # TODO: В этом методе мы должны запустить даш. Это основная точка входа в наше приложение. Реализовать запуск даша.
-    # TODO: Она должна быть подготовлена к любым неожиданностям. Должна был логика обработки критических ошибок приложения
-    def run(self):
+        
+        # Настраиваем под даш.
         global web_app
         web_app = self
-        app.run_server(debug=True)
 
+    # Метод запуска даш.
+    def run(self):
+        try:
+            app.run_server(debug=True)
+        except Exception as e:
+            print("Fatal dash, server error, restarting...")
+            self.run()
+    
+        
+    # Метод обработки входного запроса нашем приложением. 
+    # На вход подается запрос пользователя - на выход выдаются полностью проработанные ссылки полностью.
+    # TODO: Добавить сохранение в БД.
+    def process(self, user_input) -> List[SearchResult]:
+        user_request = self.interpreter.analyse_input(user_input)
+
+        links = []
+        for searcher in self.searchers:
+            searcher_links = searcher.process(user_request)
+            links.extend(searcher_links)
+
+        #print(f"Finished parsing searchers, total links: {len(links)}")
+
+        unique_links = self.get_uniquie_link_list(links)
+
+        parsed_links = []
+        for link in unique_links:
+            try:
+                searchresultmodel = self.parser_html.analyze(link, user_request)
+                if searchresultmodel != None:
+                    parsed_links.append(searchresultmodel)
+            except Exception as e:
+                print("error during parsing link", e)
+
+        # for link in parsed_links:
+        #     save_search_result(link)
+        
+        return parsed_links
+
+    # Метод обрабатывающий входной запрос из фронтенда. И выводящий таблицу, полученную в результате обработки на фронт.
     def process_query(self, n_clicks, query):
         if n_clicks is None:
             return dash.no_update
 
         # через метод process мы отправляем пользовательский запрос на дальнейшую обработку
         parsed_links = self.process(query)
-        # data = app.run(query)
-        # print(f"Finished parsing links, total results: {len(self.parsed_links)}")
-        app.css.config.serve_locally = True
-        app.scripts.config.serve_locally = True
-        app.css.append_css({
-            'external_url': app.get_asset_url('assets/typography.css')
-        })
+
         data = {
-           'URL-адрес': [],
+            'URL-адрес': [],
             'Фото': [],
-           'Регион': [],
+            'Регион': [],
             'Актуальность': [],
             'Ключевые слова': []
         }
@@ -93,122 +119,32 @@ class AcSearcher:
            data['Актуальность'].append(link.relevance)
            data['Ключевые слова'].append("Coming later")
 
-        # TODO: переписать получение данных. Пример как я обрабатывал входные данные выше. Метод веренем вам массив из SearchResultModel
         # Лучше научиться отрисовывать на основе его. Можете придумать что-то новое, можете использовать мой код, но лучше новое, крассивое
         df = pd.DataFrame(data)
 
-        # TODO: исправить таблицу и ее позиционирование и размер
-        # TODO: таблица должна показывать фотографии
         return html.Div(
             children=html.Div([
-                # html.H2("Результат"),
-                # html.P(result)
                 dash_table.DataTable(
                     id='table',
                     columns=[{'name': col, 'id': col} for col in df.columns],
                     data=df.to_dict('records'),
-                    style_header={
-                        'textAlign': 'left',
-                        'fontWeight': 'bold',
-                        'font-family':'Calibri',
-                        'background': '#f1f0fe',
-                        'padding-top': '40px',
-                        'overflow':'hidden',
-                        'table-layout': 'fixed',
-                    },
-                    style_cell={
-                        'textAlign': 'left',
-                        'font-family': 'Calibri',
-                        'background': 'white',
-                        'border-radius': '5px',
-                        'border': '1px solid #e0dfed',
-                        'background': '#f1f0fe',
-                        'padding': '40px',
-                        'table-layout': 'fixed',
-                    },
-                    style_data_conditional=[
-                        {
-                            'if': {'row_index': 'odd'},
-                            'backgroundColor': 'rgb(248, 248, 248)',
-                            'table - layout': 'fixed',
-                        },
-                        {
-                            'if': {'row_index': 'even'},
-                            'backgroundColor': 'white',
-                            'table - layout': 'fixed',
-                        },
-                        {
-                            'if': {'column_id': 'URL-адрес'},
-                            'width': '220%',
-                            'table - layout': 'fixed',
-                        },
-                        {
-                            'if': {'column_id': 'Фото'},
-                            'width': '20%',
-                            'table - layout': 'fixed',
-                        },
-                        {
-                            'if': {'column_id': 'Регион'},
-                            'width': '20%',
-                            'table - layout': 'fixed',
-                        },
-                        {
-                            'if': {'column_id': 'Актуальность'},
-                            'width': '20%',
-                            'table - layout': 'fixed',
-                        },
-                        {
-                            'if': {'column_id': 'Ключевые слова'},
-                            'width': '30%',
-                            'table - layout': 'fixed',
-                        },
-                    ]
-                )
-            ],
-                style={'margin': '0 auto', 'background': 'white', 'border-radius': '5px', 'border': '25px solid #ffffff',
-                   'background': '#f1f0fe', 'height': '100%', 'right': '10px', 'left': '10px', 'table-layout':'fixed'}
-            )
+                    style_header=style_header,
+                    style_cell=style_cell,
+                    style_data_conditional=style_data_conditional,
+                )],
+                style=style)
         )
 
-    # Метод обработки входного запроса нашем приложением. На вход подается запрос пользователя - на выход выдаются полностью проработанные ссылки полностью.
-    # TODO: упростить содержание кода, сделать его более читаемым. Извлечь некоторые циклы в методы (например поиск уникальных ссылок)
-    def process(self, user_input) -> List[SearchResult]:
-        user_request = self.interpreter.analyse_input(user_input)
-
-        links = []
-        for searcher in self.searchers:
-            searcher_links = searcher.process(user_request)
-            links.extend(searcher_links)
-
-        print(f"Finished parsing searchers, total links: {len(links)}")
-
+    # Получить уникальные ссылки
+    @staticmethod
+    def get_uniquie_link_list(links: List[Link]) -> List[Link]:
         unique_links = []
         unique_searcher_links = []
         for link in links:
             if link.url not in unique_links:
                 unique_searcher_links.append(link)
                 unique_links.append(link.url)
-
-        parsed_links = []
-        for link in unique_searcher_links:
-            try:
-                searchresultmodel = self.parser_html.analyze(link, user_request)
-                if searchresultmodel != None:
-                    parsed_links.append(searchresultmodel)
-                    print(searchresultmodel)
-            except Exception as e:
-                pass
-                print("error during parsing link", e)
-                # записб в бд
-
-        # for link in parsed_links:
-        #     save_search_result(link)
-
-        # TODO: Нужна обработка полученных ссылок, анализ на их релевантность
-
-        return parsed_links
-
-    # def callback(self, app):
+        return unique_searcher_links
 
 app.layout = html.Div(
             className="app-header",
@@ -223,4 +159,4 @@ app.layout = html.Div(
                     ]),
                 html.Td(id='result-container', className="style-for-table"),
             ]
-        )
+)
